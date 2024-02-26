cd75f804e6bd9313010f2818ec856bc398786708
Dalia Mohamed <133280610+daliaelmnofy@users.noreply.github.com>
2024-02-26 20:16:43 +0300

    Add files via upload

---
+++ Day1/Day1/Controllers/DepartmentController.cs
@@ -0,0 +1,92 @@
+﻿using Day1.Custom_Filter;
+using Day1.Models;
+using Day1.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IDepartmentRepo departmentRepo;
+
+        public DepartmentController(IDepartmentRepo _departmentRepo)
+        {
+            departmentRepo = _departmentRepo;
+        }
+
+        // GetAll | Get
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var departments = departmentRepo.GetAll();
+            return Ok(departments);
+        }
+
+        // GetById | Get
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var department = departmentRepo.GetById(id);
+            if (department == null)
+            {
+                return NotFound(new { message = $"Department with ID = {id} Not found" });
+            }
+            return Ok(department);
+        }
+
+        // GetByName | Get
+        [HttpGet("/api/Departments/Details/{name:alpha}")]
+        public IActionResult GetByName(string name)
+        {
+            var department = departmentRepo.GetByName(name);
+            if (department == null)
+            {
+                return NotFound(new { message = $"Department with Name = {name} Not found" });
+            }
+            return Ok(department);
+        }
+
+        // Add | Post
+        [HttpPost]
+        [LocationFilter("USA", "EG")] // Applying LocationFilterAttribute
+        public IActionResult Add([FromBody] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                departmentRepo.Add(department);
+                string url = Url.Link("GetOne", new { id = department.Id });
+                return Created(url, department);
+            }
+            return BadRequest(ModelState);
+        }
+
+        // Edit | Put
+        [HttpPut]
+        [LocationFilter("USA", "EG")]
+        public IActionResult Edit(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                departmentRepo.Update(department);
+                return NoContent();
+            }
+            return BadRequest();
+        }
+
+        // Delete | Delete
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var department = departmentRepo.GetById(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            departmentRepo.Delete(id);
+            return Ok(department);
+        }
+    }
+}
---
+++ Day1/Day1/Controllers/StudentController.cs
@@ -0,0 +1,93 @@
+﻿using Day1.Database;
+using Day1.Models;
+using Day1.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentRepo studentRepo;
+
+        public StudentController(IStudentRepo _studentRepo)
+        {
+            studentRepo = _studentRepo;
+        }
+
+
+
+        //GetAll | Get
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var students = studentRepo.GetAll();
+            return Ok(students);
+        }
+
+        //Get By Id | Get
+        [HttpGet("{id:int}", Name = "GetOne")]
+        public IActionResult GetById(int id)
+        {
+            var student = studentRepo.GetById(id);
+            if (student == null)
+            {
+                return NotFound(new { message = $"Student with ID = {id} Not found" });
+            }
+            return Ok(student);
+        }
+
+
+        //Get By Name | Get
+        [HttpGet("/api/Details/{name:alpha}")]
+        public IActionResult GetByName(string name)
+        {
+            var student = studentRepo.GetByName(name);
+            if (student == null)
+            {
+                return NotFound(new { message = $"Student with Name = {name} Not found" });
+            }
+            return Ok(student);
+        }
+        // Add | Post
+        [HttpPost]
+        public IActionResult Add(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                studentRepo.Add(student);
+                string url = Url.Link("GetOne", new { id = student.Id });
+                return Created(url, student);
+            }
+            return BadRequest(ModelState);
+        }
+        //Edit | Put
+        [HttpPut]
+        public IActionResult Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                studentRepo.Update(student);
+                return NoContent();
+            }
+            return BadRequest();
+        }
+
+        //Delete | Delete
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var student = studentRepo.GetById(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            studentRepo.Delete(id);
+            return Ok(student);
+        }
+    }
+
+}
